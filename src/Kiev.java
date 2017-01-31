ffbe858d39a06565797d28148e15e946857d588a
Natus Vincere <mopsd9d9pes@gmail.com>
2017-01-31 10:54:04 +0200

    commit

---
+++ src/Kiev.java
@@ -0,0 +1,8 @@
+/**
+ * Created by Natus Vincere on 31.01.2017.
+ */
+public class Kiev {
+    public static void main(String[] args) {
+        System.out.println("Ukraine");
+    }
+}
---
+++ src/dom.java
@@ -0,0 +1,9 @@
+/**
+ * Created by Natus Vincere on 30.01.2017.
+ */
+public class dom {
+    public static void main(String[] args) {
+        System.out.println("Я программист");
+
+    }
+}
