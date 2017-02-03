069e0108451040b2b127d2c80c230bdb0ff62f24
Natus Vincere <mopsd9d9pes@gmail.com>
2017-02-03 13:27:46 +0200

    commit

--- src/module2/HomeWork21d.java
+++ src/module2/HomeWork21d.java
@@ -4,13 +4,16 @@ package module2;
  * Created by Natus Vincere on 03.02.2017.
  */
 public class HomeWork21d {
-    public static void main1(String[] args) {
-        double ArrayOfDoubles[] = {7.5, 3.14, 0.1, 2.2, 3.99, 5.5, 4.9, 9.1, 8.8, 4.2};
-        double minimal = ArrayOfDoubles[3];
-        for (int i = 0; i < ArrayOfDoubles.length; i++) {
-            if (minimal > ArrayOfDoubles[i]) ;
-            minimal = ArrayOfDoubles[i];
-            System.out.println(minimal);
+    public static void main(String[] args) {
+        int [] array = {12,1,5,7,13, -1, -5};
+        int max = array[0], min = array[0];
+        for(int i = 0; i<array.length; i++){
+            if(max<array[i])
+                max = array[i];
+            if(min>array[i])
+                min = array[i];
         }
+        System.out.println("Максимальное число: "+max);
+        System.out.println("Минимальное число:"+min);
     }
-}
+    }
