4c5b96045633d738a80471b9bfd5a0af406d59f4
Natus Vincere <mopsd9d9pes@gmail.com>
2017-02-04 19:25:21 +0200

    commit

---
+++ src/module1/Fifth.java
@@ -0,0 +1,10 @@
+package module1;
+
+/**
+ * Created by Natus Vincere on 04.02.2017.
+ */
+public class Fifth {
+    public static void main(String[] args) {
+        System.out.println("Мой первый класс на Java");
+    }
+}
---
+++ src/module2/HomeWork21/HomeWork22/DZ22.java
@@ -0,0 +1,10 @@
+package module2.HomeWork21.HomeWork22;
+
+/**
+ * Created by Natus Vincere on 03.02.2017.
+ */
+public class DZ22 {
+    public static void main(String[] args) {
+
+    }
+}
