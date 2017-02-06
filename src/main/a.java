0d369de779af1463ce61dae4ae7d8226be7076a2
Natus Vincere <mopsd9d9pes@gmail.com>
2017-02-06 20:59:33 +0200

    commit

--- src/main/a.java
+++ src/main/a.java
@@ -3,5 +3,4 @@ package main;
 /**
  * Created by Natus Vincere on 06.02.2017.
  */
-public class a {
-}
+public class a {}
--- src/main/java/com/Alexandr/practice/modul02/task001/Dz.java
+++ src/main/java/com/Alexandr/practice/modul02/task001/Dz.java
@@ -20,67 +20,66 @@ public class Dz {
         Array1[0] = -2.5;
         Array1[1] = 4.1;
         Array1[2] = 2.1;
-        Array1[3] = 1.1;
+        Array1[3] = 1.2;
         Array1[4] = 5.1;
         Array1[5] = 1.1;
         Array1[6] = 4.1;
-        Array1[7] = 1.9;
+        Array1[7] = 2.9;
         Array1[8] = 3.2;
         Array1[9] = -1.2;
 
-
-        System.out.println("Сумма всех целых чисел: " + sum(Array));
-        System.out.println("Сумма всех дробных чисел: " + sum(Array1));
-        System.out.println("Минимальное целое число: " + min(Array));
-        System.out.println("Минимальное дробное число: " + min(Array1));
-        System.out.println("Максимальное число: " + max(Array));
-        System.out.println("Максимальное дробное число: " + max(Array1));
-        System.out.println("Максимальное позитивное число: " + maxPositive(Array));
-        System.out.println("Максимальное позитивное дробное число: " + maxPositive(Array1));
-        System.out.println("Multiplication for int: " + multiplication(Array));
-        System.out.println("Multiplication for double: " + multiplication(Array1));
-        System.out.println("Модуль первого целого числа: " + modulus(Array));
-        System.out.println("Модуль последнего дробного числа: " + modulus(Array1));
-        System.out.println("Модуль последнего целого числа: "+modulus1(Array));
-        System.out.println("Модуль первого дробного числа: "+modulus1(Array1));
-        System.out.println("Второе целое число: "+secondLargest(Array));
-        System.out.println("Второе дробное число: "+secondLargest(Array1));
+        Dz DZ = new Dz();
+        DZ.sum(Array);
+        DZ.sum(Array1);
+        DZ.min(Array);
+        DZ.min(Array1);
+        DZ.max(Array);
+        DZ.max(Array1);
+        DZ.maxPositive(Array);
+        DZ.maxPositive(Array1);
+        DZ.multiplication(Array);
+        DZ.multiplication(Array1);
+        DZ.modulus(Array);
+        DZ.modulus(Array1);
+        DZ.secondLargest(Array);
+        DZ.secondLargest(Array1);
 
 
     }
 
 
-    public static int sum(int Array[]) {
+    public void sum(int Array[]) {
 
         int a = 0;
-        for (int i : Array) {
+        for (int i : Array)
             a += i;
+        System.out.println("Сумма всех целых чисел: " + a);
 
 
-        }
-        return a;
     }
 
-    public static double sum(double Array1[]) {
-        int b = 0;
-        for (double ball : Array1) {
+    public void sum(double Array1[]) {
+        double b = 0;
+        for (double ball : Array1)
             b += ball;
-        }
-        return b;
+        System.out.println("Сумма всех дробных чисел: " + b);
+
+
     }
 
-    public static int min(int Array[]) {
-        int a = Array[0];
+    public void min(int Array[]) {
+        int c = Array[0];
         for (int i = 0; i < Array.length; i++) {
-            if (a > Array[i])
-                a = Array[i];
+            if (c > Array[i])
+                c = Array[i];
+
         }
-        return a;
+        System.out.println("Минимальное целое число: " + c);
 
 
     }
 
-    public static double min(double Array1[]) {
+    public void min(double Array1[]) {
         double a = Array1[0];
         for (int i = 0; i < Array1.length; i++) {
             if (a > Array1[i])
@@ -88,21 +87,23 @@ public class Dz {
 
 
         }
-        return a;
+        System.out.println("Минимальное дробное число: " + a);
+
 
     }
 
-    public static int max(int Array[]) {
+    public void max(int Array[]) {
         int a = Array[0];
         for (int i = 0; i < Array.length; i++) {
             if (a < Array[i])
                 a = Array[i];
 
         }
-        return a;
+        System.out.println("Максимальное число: " + a);
+
     }
 
-    public static double max(double Array1[]) {
+    public void max(double Array1[]) {
         double a = Array1[0];
         for (int i = 0; i < Array1.length; i++) {
             if (a < Array1[i])
@@ -110,107 +111,97 @@ public class Dz {
 
 
         }
-        return a;
+        System.out.println("Максимальное дробное число: " + a);
+
     }
 
-    public static int maxPositive(int Array[]) {
+    public void maxPositive(int Array[]) {
         int a = 0;
         for (int number : Array) {
             if (number > 0 && a < number)
                 a = number;
         }
-        return a;
+        System.out.println("Максимальное позитивное число: " + a);
     }
 
-    public static double maxPositive(double Array1[]) {
+    public void maxPositive(double Array1[]) {
         double a = 0;
         for (double number : Array1) {
             if (number > 0 && a < number)
                 a = number;
         }
-        return a;
+        System.out.println("Максимальное дробное позитивное число: " + a);
 
     }
 
-    public static int multiplication(int Array[]) {
+    public void multiplication(int Array[]) {
         int mult = 1;
         for (int number : Array) {
             mult *= number;
 
         }
-        return mult;
+        System.out.println("Multiplication for int: " + mult);
     }
 
-    public static double multiplication(double Array1[]) {
+    public void multiplication(double Array1[]) {
         double mult = 1;
         for (double number : Array1) {
             mult *= number;
         }
-        return mult;
+        System.out.println("Multiplication for double: " + mult);
 
     }
 
-    public static int modulus(int Array[]) {
+    public void modulus(int Array[]) {
 
         int first = Array[0];
         int last = Array[Array.length - 1];
-        {
-        }
-        return Math.abs(first);
+
+        System.out.println("Модуль первого числа: " + Math.abs(first));
+        System.out.println("Модуль ппоследнего числа: " + Math.abs(last));
 
 
     }
 
-    public static double modulus(double Array[]) {
+    public void modulus(double Array[]) {
 
         double first = Array[0];
         double last = Array[Array.length - 1];
-        {
-        }
-        return Math.abs(last);
+
+        System.out.println("Модуль первого дробного числа: " + Math.abs(first));
+        System.out.println("Модуль последнего дробного числа: " + Math.abs(first));
 
     }
 
-    public static int modulus1(int Array[]) {
 
-        int first = Array[0];
-        int last = Array[Array.length - 1];
-        {
-        }
-        return Math.abs(last);
-    }
-    public static double modulus1(double Array[]) {
-
-        double first = Array[0];
-        double last = Array[Array.length - 1];
-        {
-        }
-        return Math.abs(first);
-
-    }
-    public static int secondLargest (int Array[]){
+    public void secondLargest(int Array[]) {
         int Largest = Array[0];
         int SecondLargest = Array[0];
-        for(int number : Array)
-            if(number > Largest){
+        for (int number : Array)
+            if (number > Largest) {
                 SecondLargest = Largest;
                 Largest = number;
 
-            }else if(number > SecondLargest)
-                SecondLargest = number;{} return SecondLargest;
+            } else if (number > SecondLargest)
+                SecondLargest = number;
+        System.out.println("Второе целое число: " + SecondLargest);
     }
-    public static double secondLargest (double Array1[]){
+
+    public void secondLargest(double Array1[]) {
         double Largest = Array1[0];
         double SecondLargest = Array1[0];
-        for(double number : Array1)
-            if(number > Largest){
+        for (double number : Array1)
+            if (number > Largest) {
                 SecondLargest = Largest;
                 Largest = number;
 
-            }else if(number > SecondLargest)
-                SecondLargest = number;{} return SecondLargest;
+            } else if (number > SecondLargest)
+                SecondLargest = number;
+        System.out.println("Второе дробное число: " + SecondLargest);
+
     }
 }
+
 
 
 
