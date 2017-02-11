a2df124b24e5d912e6177f5bcdfef6b0b350463d
Natus Vincere <mopsd9d9pes@gmail.com>
2017-02-11 13:18:35 +0200

    commit

---
+++ src/main/java/com/Alexandr/practice/modul03/task001/Bird.java
@@ -0,0 +1,26 @@
+package main.java.com.Alexandr.practice.modul03.task001;
+
+/**
+ * Created by Natus Vincere on 11.02.2017.
+ */
+public class Bird {
+    public static void main(String[] args) {
+        Bird birdDyatel = new Bird();
+        birdDyatel.sing();
+
+    }
+
+
+    void sing (){
+        System.out.println("I am walking");
+        System.out.println("I am flying");
+        System.out.println("I am singing");
+        System.out.println("I am Bird");
+    }
+
+
+
+}
+
+
+
