1a5cd6ba249ca9049a4e439f6441e7689f8bf213
admin <mopsd9d9pes@gmail.com>
2017-02-24 16:24:45 +0200

    Commit

---
+++ src/main/java/com/Alexandr/practice/modul04/task01/Bank.java
@@ -0,0 +1,100 @@
+package main.java.com.Alexandr.practice.modul04.task01;
+
+import java.util.Currency;
+
+/**
+ * Created by admin on 24.02.2017.
+ */
+public abstract class Bank {
+    long id;
+    String bankCountry;
+    Currency currency;
+    int numberOfEmployees;
+    double avrSalaryOfEmployee;
+    long rating;
+    long totalCapital;
+
+    public Bank(long id, String bankCountry, Currency currency, int numberOfEmployees, double avrSalaryOfEmployee, long rating, long totalCapital) {
+        this.id = id;
+        this.bankCountry = bankCountry;
+        this.currency = currency;
+        this.numberOfEmployees = numberOfEmployees;
+        this.avrSalaryOfEmployee = avrSalaryOfEmployee;
+        this.rating = rating;
+        this.totalCapital = totalCapital;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getBankCountry() {
+        return bankCountry;
+    }
+
+    public Currency getCurrency() {
+        return currency;
+    }
+
+    public int getNumberOfEmployees() {
+        return numberOfEmployees;
+    }
+
+    public double getAvrSalaryOfEmployee() {
+        return avrSalaryOfEmployee;
+    }
+
+    public long getRating() {
+        return rating;
+    }
+
+    public long getTotalCapital() {
+        return totalCapital;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public void setBankCountry(String bankCountry) {
+        this.bankCountry = bankCountry;
+    }
+
+    public void setCurrency(Currency currency) {
+        this.currency = currency;
+    }
+
+    public void setNumberOfEmployees(int numberOfEmployees) {
+        this.numberOfEmployees = numberOfEmployees;
+    }
+
+    public void setAvrSalaryOfEmployee(double avrSalaryOfEmployee) {
+        this.avrSalaryOfEmployee = avrSalaryOfEmployee;
+    }
+
+    public void setRating(long rating) {
+        this.rating = rating;
+    }
+
+    public void setTotalCapital(long totalCapital) {
+        this.totalCapital = totalCapital;
+    }
+    abstract int getLimitOfWithdrawal();
+
+
+    abstract int getLimitOfFunding();
+
+
+    abstract int getMonthlyRate();
+
+
+    abstract int getCommission(int summ);
+
+
+    double moneyPaidMonthlyForSalary(){
+        return 0;
+    }
+
+    public Bank() {
+    }
+}
---
+++ src/main/java/com/Alexandr/practice/modul04/task01/ChinaBank.java
@@ -0,0 +1,7 @@
+package main.java.com.Alexandr.practice.modul04.task01;
+
+/**
+ * Created by admin on 24.02.2017.
+ */
+public abstract class ChinaBank extends Bank {
+}
